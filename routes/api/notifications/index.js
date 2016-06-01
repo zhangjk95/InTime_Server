dc1d7ee9b61f553c88b137c0a678909cf7dd1b97
zhangjk95 <zjk_261011@163.com>
2016-06-01 15:37:01 +0800

    Update notifications

--- routes/api/notifications/index.js
+++ routes/api/notifications/index.js
@@ -21,7 +21,7 @@ router.get('/', function(req, res, next) {
 
 // PUT /notifications/all
 router.put('/all', function(req, res, next) {
-    if (!(req.body.read instanceof Boolean)) {
+    if (typeof(req.body.read) !== "boolean") {
         return res.status(400).json({ error: 'Type of "read" must be boolean.' });
     }
 
