7dfb64fdfacbe7b610f19241e364c24807d38cb0
v_zhangjiakai <v_zhangjiakai@baidu.com>
2016-07-20 12:56:14 +0800

    Update transactions

--- routes/webview/transactions/index.js
+++ routes/webview/transactions/index.js
@@ -5,7 +5,20 @@ var router = require('express').Router();
 // GET /transactions
 router.get('/', function(req, res, next) {
     request(req, res, '/transactions', req.query).then(function (data) {
+
+        data.transactions.forEach(function (transaction) {
+            switch (transaction.type.split('/')[0]) {
+                case 'order':
+                    transaction.href = '/activities/order_detail?oid=' + transaction.details.oid;
+                    break;
+                default:
+                    transaction.href = '/test';
+                    break;
+            }
+        });
+
         res.render('transactions', data);
+
     }).catch(function (err) {
         next(err);
     })
