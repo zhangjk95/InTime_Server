6f3aabf2ac8d411f0ccb875ef0a8f9da43284163
v_zhangjiakai <v_zhangjiakai@baidu.com>
2016-07-20 15:41:25 +0800

    Update transactions

--- routes/api/transactions/index.js
+++ routes/api/transactions/index.js
@@ -6,14 +6,19 @@ var Transaction = require(__base + 'models/transaction');
 
 // GET /transactions
 router.get('/', function(req, res, next) {
-    Transaction.find({ uid: ObjectId(req.user.uid) }, function(err, transactions) {
+    Transaction.aggregate([
+        { $match: { uid: ObjectId(req.user.uid) } },
+        { $lookup: { from: "orders", localField: "details.oid", foreignField: "_id", as: "order" }},
+        { $unwind: { path: '$order', preserveNullAndEmptyArrays: true } }
+    ], function(err, transactions) {
         if (err) return next(err);
 
         return res.json({ transactions: transactions.map((transaction) => ({
             trid: transaction._id,
             type: transaction.type,
             total: transaction.total,
-            details: transaction.details
+            details: transaction.details,
+            order: transaction.order
         })) });
     });
 });
